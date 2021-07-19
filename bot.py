8eabe7d704509f4136728fef8ccd0bbaabd7cd0b
xiang880819 <50402595+xiang880819@users.noreply.github.com>
2021-07-19 16:21:51 +0800

    add join,leave,ping

--- bot.py
+++ bot.py
@@ -1,11 +1,31 @@
 import discord
+from discord import channel
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix=';')
+intents = discord.Intents.default()
+intents.members = True
+bot = commands.Bot(command_prefix=';', intents=intents)
 
 
 @bot.event
 async def on_ready():
     print(">> Bot is online <<")
 
-bot.run('ODY2NTgxOTIyMzg1MzYyOTQ2.YPUpTg.zQbFdGJDvn_IFB4nw2V_BYxs9WA')
+
+@bot.event
+async def on_member_join(member):
+    channel = bot.get_channel(866593287749632030)
+    await channel.send(f'{member} join!')
+
+
+@bot.event
+async def on_member_remove(member):
+    channel = bot.get_channel(866593287749632030)
+    await channel.send(f'{member} leave!')
+
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f'{round(bot.latency*1000)} (ms)')
+
+bot.run('ODY2NTgxOTIyMzg1MzYyOTQ2.YPUpTg.Ue-qgCQ7QiMrjTTzH6FYqM65_DI')

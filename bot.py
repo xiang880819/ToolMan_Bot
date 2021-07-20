31b747954c808cbb63c2346a46cfffe6b75150c9
xiang880819 <50402595+xiang880819@users.noreply.github.com>
2021-07-20 18:33:40 +0800

    add pic,gif,web

--- bot.py
+++ bot.py
@@ -1,4 +1,6 @@
 import discord
+import random
+from discord import file
 from discord.ext import commands
 import json
 
@@ -31,5 +33,24 @@ async def on_member_remove(member):
 @bot.command()
 async def ping(ctx):
     await ctx.send(f'{round(bot.latency*1000)} (ms)')
+
+
+@bot.command()
+async def 派大星(ctx):
+    random_pic = random.choice(jdata['pic'])
+    pic = discord.File(random_pic)
+    await ctx.send(file=pic)
+
+
+@bot.command()
+async def shake(ctx):
+    random_gif = random.choice(jdata['shake'])
+    gif = discord.File(random_gif)
+    await ctx.send(file=gif)
+
+
+@bot.command()
+async def av(ctx):
+    await ctx.send('https://jable.tv')
 
 bot.run(jdata['TOKEN'])
